1c40491284f78d4d472048160d1343d0fcf95943
AokiMakoto <aokimakoto0322@gmail.com>
2026-05-27 22:12:45 +0900

    robots.txtにllm.jsonの記載を削除

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,19 +1,17 @@
 import type { APIRoute } from "astro";
 
-const getRobotsTxt = (sitemapURL: URL, llmJsonURL: URL, llmTxtURL: URL) => `
+const getRobotsTxt = (sitemapURL: URL, llmTxtURL: URL) => `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
-Sitemap: ${llmJsonURL.href}
 Sitemap: ${llmTxtURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL("sitemap-index.xml", site);
 
-  const llmJsonURL = new URL("llm.json", site);
   const llmTxtURL = new URL("llm.txt", site);
 
-  return new Response(getRobotsTxt(sitemapURL, llmJsonURL, llmTxtURL));
+  return new Response(getRobotsTxt(sitemapURL, llmTxtURL));
 };
